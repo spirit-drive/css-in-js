0e6480e768325c5b4149753615d6d54b0f600fb8
Igor <spirit-drive@yandex.ru>
2022-07-18 23:15:46 +0300

    little improvements

--- src/components/CssExample/CssExample.tsx
+++ src/components/CssExample/CssExample.tsx
@@ -10,7 +10,7 @@ export const CssExample: FC<ButtonProps> = ({ className, ...props }) => {
   const [opened, setOpened] = useState<boolean>(false);
   return (
     <div className={cn('css-example', className)}>
-      <button className={'css-example__button'} {...props} onClick={() => setOpened(v => !v)} />
+      <button className="css-example__button" {...props} onClick={() => setOpened(v => !v)} />
       <div className={cn('css-example__text', opened ? 'css-example__text_opened' : 'css-example__text_closed')}>Test</div>
       <div className={cn('css-example__text', !opened && 'css-example__text_closed')}>Test</div>
     </div>
